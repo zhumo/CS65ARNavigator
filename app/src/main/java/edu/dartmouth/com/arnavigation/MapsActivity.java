9f878ba5e84d6929f154a5f0c6ec49fc7f2550a9
Mo Zhu <mozhu888@gmail.com>
2018-02-11 15:14:11 -0500

    ensure user gets prompted for permission, even if they take it away mid-use

--- app/src/main/java/edu/dartmouth/com/arnavigation/MapsActivity.java
+++ app/src/main/java/edu/dartmouth/com/arnavigation/MapsActivity.java
@@ -1,11 +1,17 @@
 package edu.dartmouth.com.arnavigation;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,24 +28,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        if(hasLocationPermission()) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        } else {
+            launchPermissionsActivity();
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(!hasLocationPermission()) { launchPermissionsActivity(); }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -59,5 +66,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 lastKnownLocation.getLongitude()
         );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnownLatLng, 17.0f));
+    }
+
+    private void launchPermissionsActivity() {
+        Intent permissionsActivityIntent = new Intent(this, PermissionsActivity.class);
+        permissionsActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(permissionsActivityIntent);
+    }
+
+
+    private boolean hasLocationPermission() {
+        if(Build.VERSION.SDK_INT < 23) { return true; }
+
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 }
