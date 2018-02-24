c39615e94921cb448beb365dc97516a03901d638
Mo Zhu <mozhu888@gmail.com>
2018-02-24 18:41:30 -0500

    extract permission system into PermissionManager

--- app/src/main/java/edu/dartmouth/com/arnavigation/MapsActivity.java
+++ app/src/main/java/edu/dartmouth/com/arnavigation/MapsActivity.java
@@ -2,15 +2,19 @@ package edu.dartmouth.com.arnavigation;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 
@@ -25,33 +29,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private LocationManager locationManager;
 
+    private static int LOCATION_PERMISSION_REQUEST_CODE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        if(hasLocationPermission()) {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        } else {
-            launchPermissionsActivity();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if(!hasLocationPermission()) { launchPermissionsActivity(); }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        PermissionManager.ensurePermission(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                this,
+                LOCATION_PERMISSION_REQUEST_CODE,
+                new PermissionManager.OnHasPermission() {
+                    @Override
+                    public void onHasPermission() { setupMap();}
+                }
+        );
+    }
+
+    private void setupMap() {
         // Ignore this error because onCreate ensures location permission exists.
         mMap.setMyLocationEnabled(true);
 
@@ -69,20 +74,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnownLatLng, 17.0f));
     }
 
-    private void launchPermissionsActivity() {
-        Intent permissionsActivityIntent = new Intent(this, PermissionsActivity.class);
-        permissionsActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(permissionsActivityIntent);
-    }
-
-    public void cameraButtonClicked(View view) {
+    public void activateCamera(View view) {
         Intent cameraActivityIntent = new Intent(this, CameraActivity.class);
         startActivity(cameraActivityIntent);
     }
 
-    private boolean hasLocationPermission() {
-        if(Build.VERSION.SDK_INT < 23) { return true; }
-
-        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if(resultCode == Activity.RESULT_OK) { setupMap(); }
+            else { finish(); }
+        }
     }
 }
