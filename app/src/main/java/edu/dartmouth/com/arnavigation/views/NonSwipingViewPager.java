ca4c164391f86c51043d7a351afcbeb7e5089da7
Mo Zhu <mozhu888@gmail.com>
2018-02-28 12:01:52 -0500

    fix view pager

--- app/src/main/java/edu/dartmouth/com/arnavigation/views/NonSwipingViewPager.java
+++ app/src/main/java/edu/dartmouth/com/arnavigation/views/NonSwipingViewPager.java
@@ -2,11 +2,15 @@ package edu.dartmouth.com.arnavigation.views;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 public class NonSwipingViewPager extends ViewPager {
 
     public NonSwipingViewPager(Context context) { super(context); }
+    public NonSwipingViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
