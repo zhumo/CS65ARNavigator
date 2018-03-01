fd2d7775833f39cbeeda5cf2bf6fbbd8ce0377b6
Mo Zhu <mozhu888@gmail.com>
2018-02-28 22:55:09 -0500

    pause rendering when CameraFragment is not active ViewPage

--- app/src/main/java/edu/dartmouth/com/arnavigation/view_pages/CameraFragment.java
+++ app/src/main/java/edu/dartmouth/com/arnavigation/view_pages/CameraFragment.java
@@ -199,16 +199,12 @@ public class CameraFragment extends Fragment implements GLSurfaceView.Renderer {
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
+        if(mSurfaceView == null) { return; }
+
         if (isVisibleToUser) {
-//            // Standard Android full-screen functionality.
-//            getActivity().getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            mSurfaceView.onResume();
+        } else {
+            mSurfaceView.onPause();
         }
     }
 
